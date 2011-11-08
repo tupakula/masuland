3d2913dad2bde10db643ecd0b7786397c7f6cc0c
masuland <masuland@1601dfa1-f7e5-e4cb-2ed4-966c340d0d91>
2011-11-08 08:08:06 +0000

    Initial import.

---
+++ trunk/LoginExampleServerAmfPHP_1.9/trunk/services/AppMySqlDAO.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once "./com/masuland/loginexample/data/dto/AuthenticationDTO.php";
+require_once "./com/masuland/loginexample/data/dto/UserDTO.php";
+
+/**
+ * @author masuland.com
+ */
+class AppMySqlDAO
+{
+	/**
+	 * testService()
+	 * 
+	 * @return Boolean
+	 */
+	public function testService() 
+	{
+		return false;
+	}
+	
+	/**
+	 * getSettings()
+	 * 
+	 * @return SettingsDTO
+	 */
+	public function getSettings()
+	{
+		return null;
+	}
+
+	/**
+	 * login()
+	 * 
+	 * @param auth
+	 * @return UserDTO
+	 */
+	public function login(AuthenticationDTO $auth) 
+	{
+		return null;
+	}
+	
+	/**
+	 * register()
+	 * 
+	 * @param auth
+	 * @return UserDTO
+	 */
+	public function register(AuthenticationDTO $auth) 
+	{
+		return null;
+	}
+	
+	/**
+	 * updateUser()
+	 * 
+	 * @param user
+	 * @return UserDTO
+	 */
+	public function updateUser(UserDTO $user) 
+	{
+		return null;
+	}
+}
+
+?>
---
+++ trunk/LoginExampleServerAmfPHP_1.9/trunk/services/com/masuland/loginexample/data/dto/SettingsDTO.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author masuland.com
+ */
+class SettingsDTO
+{	
+	var $_explicitType = "com.masuland.loginexample.data.dto.SettingsDTO";
+	
+	var $layouts; // List of LayoutDTOs
+}
+
+?>
